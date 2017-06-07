f2aa6b71bfba7135c9db8852a010a33679ab04e0
rom58 <josjunk@gmail.com>
2017-06-07 15:07:31 -0600

    cleaning up project files

--- ecel_gui.py
+++ ecel_gui.py
@@ -1,12 +1,12 @@
 import gtk
 import signal
 from engine.engine import Engine
-from gui.ecel_gui import EcelGUI
+from gui.main_gui import MainGUI
 
 engine = Engine()
 
 if __name__ == "__main__":
     gtk.threads_init()
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    EcelGUI(engine)
+    MainGUI(engine)
     gtk.main()
