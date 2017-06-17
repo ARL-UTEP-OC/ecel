c0abfcb84e681422bc84e2c4fad2f15923926f97
Kevin Keenan <kpkeenan2493@gmail.com>
2017-06-17 17:48:23 -0400

    Linked to Issue #25
          Added empty nmap class
          Added config files

---
+++ plugins/collectors/nmap/nmap.py
@@ -0,0 +1,9 @@
+import os
+from engine.collector import AutomaticCollector
+
+class nmap(AutomaticCollector):
+
+
+    def build_commands(self):
+        a=7;
+        #TODO. ignore above statement....
