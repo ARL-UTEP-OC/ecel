616d9f52e59a5d30294cd5e14b5fc609e51dec53
Kevin Keenan <kpkeenan@miners.utep.edu>
2017-06-19 23:31:04 -0600

    Linked to Issue #25
        Enabled nmap software to run simple command as      collector. Runs individually and is included       when all collectors are ran.
    
        Added configuration options for
        nmap
    
        TODO: Add schedulability
    
        TODO: Discuss which nmap parameters should be     executed when building commands.

--- plugins/collectors/nmap/nmap.py
+++ plugins/collectors/nmap/nmap.py
@@ -1,9 +1,19 @@
 import os
+import definitions
 from engine.collector import AutomaticCollector
 
 class nmap(AutomaticCollector):
 
 
     def build_commands(self):
-        a=7;
-        #TODO. ignore above statement....
+        option = self.config.get_collector_custom_data()["interfaces"]["options"]
+        network = self.config.get_collector_custom_data()["interfaces"]["network"]
+
+        out_file_name = "nmap_" + definitions.TIMESTAMP_PLACEHOLDER
+        self.output_filenames.append(out_file_name)
+        out_file_path = os.path.join(self.output_dir, out_file_name + ".txt")
+
+        # build commands
+        cmd = self.name + option + network;
+        self.commands.append(cmd)
+
