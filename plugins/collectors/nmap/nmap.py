b14e488e2c1c1c52cd1422799604b274a1beb503
Kevin Keenan <kpkeenan@miners.utep.edu>
2017-06-20 22:40:50 -0600

    Linked to Issue #25
        Modified properties in config json file
    
        nmap stores output in .txt file stored in raw directory.

--- plugins/collectors/nmap/nmap.py
+++ plugins/collectors/nmap/nmap.py
@@ -6,14 +6,14 @@ class nmap(AutomaticCollector):
 
 
     def build_commands(self):
-        option = self.config.get_collector_custom_data()["interfaces"]["options"]
-        network = self.config.get_collector_custom_data()["interfaces"]["network"]
+        option = self.config.get_collector_custom_data()["interfaces"]["additional options"]
+        network = self.config.get_collector_custom_data()["interfaces"]["ip range"]
 
         out_file_name = "nmap_" + definitions.TIMESTAMP_PLACEHOLDER
         self.output_filenames.append(out_file_name)
         out_file_path = os.path.join(self.output_dir, out_file_name + ".txt")
 
         # build commands
-        cmd = self.name + option + network;
+        cmd = self.name + option + network + " -oN " + out_file_path
         self.commands.append(cmd)
 
