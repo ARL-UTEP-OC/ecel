6702ff4703ee0298c1f9a4e3d8b46b48a610b72f
Kevin Keenan <kpkeenan@miners.utep.edu>
2017-06-21 23:48:37 -0600

    Linked to Issue #26
        Continued to set up nmap_parser.py
        Adjusted nmap collector config to point to nmap parser class
        Added empty bat/shell nmap parser files.

--- plugins/parsers/nmap/nmap_parser.py
+++ plugins/parsers/nmap/nmap_parser.py
@@ -8,4 +8,16 @@ class NMapParser(Parser):
 
     def __init__(self, collector):
         super(NMapParser, self).__init__(collector)
+        if os.name == 'nt':
+            self.script_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "nmap_parser.bat")
+        else:
+            self.script_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "nmap_parser.sh")
 
+    def parse(self):
+        if os.name == 'nt':
+            subprocess.Popen(
+                [self.script_file, self.file_or_dir, self.parsed_folder],
+                cwd=os.path.dirname(os.path.realpath(__file__)),
+                stdout=subprocess.PIPE, shell=True, stderr=subprocess.PIPE)
+        else:
+            subprocess.call([self.script_file, self.file_or_dir, self.parsed_folder], shell=False)
