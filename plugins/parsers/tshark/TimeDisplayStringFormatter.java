34e958909c7df5344bc9adbd4a3279aae19acd5e
raistlinJ <raistlinJ@users.noreply.github.com>
2017-07-19 23:02:21 -0400

    Tshark parser now completes in one pass. Changed throughput JSON to use 'start' instead of 'x' for consistency

--- plugins/parsers/tshark/TimeDisplayStringFormatter.java
+++ plugins/parsers/tshark/TimeDisplayStringFormatter.java
@@ -6,6 +6,125 @@ public class TimeDisplayStringFormatter {
 	static HashSet<String> uniqueElementsPerWindow = new HashSet<String>();
     static long numXYItems = 0;
     static long numProtoItems = 0;
+
+		public static void writeJSONFiles(String buffer, double windowSize, String outputDirectory) {
+		String protocolJSON = "[";
+		String throughputJSON = "[";
+		String[] lines = buffer.split(System.getProperty("line.separator"));
+
+		double currWindowStartTime = 0.0;
+		double currPacketTime = 0.0;
+		int packetsPerSecond = 0;
+		String tempString;
+		String[] parsedPacket;
+		String timestamp;
+		long packetsInWindow = 1;
+		SimpleDateFormat toFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		toFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		try {						
+			//first grab the time_epoch from the line:
+			if(lines.length > 1)
+			{
+				currWindowStartTime = Double.parseDouble(lines[0].split(" ",-1)[PacketData.Fields.FRAME_TIMEEPOCH.ordinal()]);
+
+			for (int i=0; i<lines.length; i++)
+			{
+				//System.out.println("line: " + lines[i]);
+				parsedPacket = lines[i].split(" ",-1);
+				currPacketTime = Double.parseDouble(parsedPacket[PacketData.Fields.FRAME_TIMEEPOCH.ordinal()]);;
+				
+				//This is the case when the data is still within the window size threshold, and there are still data left
+				if((currPacketTime - currWindowStartTime) < windowSize && i != lines.length-1)
+				{
+					packetsInWindow++;
+					//remove any unwanted elements from the array before storing in the map
+					//System.out.println(parsedPacket[PacketData.Fields.FRAME_TIMEEPOCH.ordinal()] );
+					lines[i] = lines[i].replace(parsedPacket[PacketData.Fields.FRAME_TIMEEPOCH.ordinal()], "");
+					//lines[i] = lines[i].replace(parsedPacket[PacketData.Fields.ETH_SRC.ordinal()], "");
+					//lines[i] = lines[i].replace(parsedPacket[PacketData.Fields.ETH_DST.ordinal()], "");
+					//System.out.println(lines[i] + "\nremoved: " + parsedPacket[PacketData.Fields.FRAME_TIMEEPOCH.ordinal()] + "\n");
+					uniqueElementsPerWindow.add(lines[i]+"\n");
+				}
+				//The data is part of a new window because the log time is larger than the window size
+				else
+				{
+				    timestamp = toFormat.format(new Date((long)(currWindowStartTime*1000)));
+					//System.out.println("Starting new window");
+
+					//////////////////////Append to Protocol JSON//////////////////////
+					protocolJSON += "{ \"traffic_all_id\" : "+(numProtoItems++)+", ";
+					protocolJSON += "\"content\" :\"";
+					packetsPerSecond = (int)(packetsInWindow/windowSize);
+					if(packetsPerSecond<1)
+						protocolJSON+=quote("<1 p/s");
+					else protocolJSON+=quote(packetsPerSecond+" p/s");
+					protocolJSON += "\", ";
+					
+					protocolJSON += "\"className\" : \"traffic";
+					protocolJSON += "\", ";
+					
+					protocolJSON += "\"title\" : \"";
+					for(String element : uniqueElementsPerWindow)
+					{
+						tempString = element.replace(";","\n");
+						tempString = quote(tempString);
+						protocolJSON += tempString;
+					}
+					protocolJSON += "\", ";
+										
+					protocolJSON += "\"start\" : \"" + timestamp;
+					if(i == lines.length-1){
+						protocolJSON += "\" }\n";
+					}
+					else{
+						protocolJSON += "\" },\n";
+					}
+
+
+					//////////////////////Append to Throughput JSON//////////////////////
+					throughputJSON += "{ \"traffic_xy_id\" : "+(numXYItems++)+", ";
+					throughputJSON += "\"className\" : \"trafficThroughput";
+					throughputJSON += "\", ";
+					throughputJSON += "\"start\":\"" + timestamp;
+					throughputJSON += "\", ";
+					
+					throughputJSON += "\"y\" : ";
+					packetsPerSecond = (int)(packetsInWindow/windowSize);
+					throughputJSON+=packetsPerSecond;
+					if(i == lines.length-1){
+						throughputJSON += " }\n";
+					}
+					else{
+						throughputJSON += " },\n";
+					}
+
+
+					currWindowStartTime = currPacketTime;
+					uniqueElementsPerWindow.clear();
+					packetsInWindow = 1;
+					//remove any unwanted elements from the array before storing in the map
+					
+					lines[i] = lines[i].replace(parsedPacket[PacketData.Fields.FRAME_TIMEEPOCH.ordinal()], "");
+					//lines[i] = lines[i].replace(parsedPacket[PacketData.Fields.ETH_SRC.ordinal()], "");
+					//lines[i] = lines[i].replace(parsedPacket[PacketData.Fields.ETH_DST.ordinal()], "");
+					//System.out.println("removed: " + parsedPacket[PacketData.Fields.FRAME_TIMEEPOCH.ordinal()] + "\n");
+					uniqueElementsPerWindow.add(lines[i]+"\n");
+				} 
+			}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		protocolJSON += "]";
+		throughputJSON += "]";
+		
+		//Write to the output folder
+		FileOutput.WriteToFile(outputDirectory+"/networkDataAll.JSON", protocolJSON);
+		FileOutput.WriteToFile(outputDirectory+"/networkDataXY.JSON", throughputJSON);
+	}
+
+
+
 	public static String formatXMLString(String buffer, double windowSize) {
 		String answer = "<?xml version='1.0' encoding='UTF-8'?>\n"+
 					"<data>\n";
