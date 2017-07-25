8e50f5dca06ead0e6aff40459ea47261e43d50c9
Kevin Keenan <kpkeenan@miners.utep.edu>
2017-07-24 21:58:31 -0600

    Linked to Issue #29
    Creates plugin configs/shared variables
    Checks that cmd is running as admin
    Compiles parsers (must add hardcoded location of javac exe to path)

--- install.bat
+++ install.bat
@@ -1,4 +1,27 @@
-REM Make sure path to pip is set correctly
-pip install virtualenv
-virtualenv ecel-installer
-ecel-installer\Scripts\activate & pip install gobject & pip install psutil & pip install python-xlib & pip install dpkt & pip install schedule & pip install netifaces
+cls
+REM Make sure path to pip is set correctly and CMD.exe is running with administrative privileges.
+net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Installing ECEL for windows...
+    ) else (
+        echo Please run cmd as an admin
+    )
+
+SET "ECEL_DIR=%cd%"
+SET JAVAC_DIR=C:\Program Files\Java\jdk1.8.0_91\bin
+SET OUTPUT_PREFIX=ECEL INSTALLER:
+SET OUTPUT_ERROR_PREFIX=%OUTPUT_PREFIX% ERROR:
+
+
+echo Creating Plugin Configs
+for /D %%d in (.\plugins\collectors\*) do copy %%d\config.json.template %%d\config.json & copy %%d\config_schema.json.template %%d\config_schema.json
+
+echo %OUTPUT_PREFIX% Compiling parsers
+set path="%path%;%JAVAC_DIR%;"
+for /D %%d in (.\plugins\parsers\*) do if exist %%d\*.java (javac %%d\*.java)
+
+
+
+
+echo %OUTPUT_PREFIX% Installation Complete.
+
