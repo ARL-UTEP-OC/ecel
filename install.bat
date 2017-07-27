f31ab71b1426dd833e19e7e9d43c8f4068cfe1b0
Kevin Keenan <kpkeenan@miners.utep.edu>
2017-07-26 22:56:51 -0600

    Linked to Issue #29
    Added comments to bat file.

--- install.bat
+++ install.bat
@@ -1,30 +1,27 @@
 REM Make sure path to pip is set correctly and CMD.exe is running with administrative privileges.
-net session >nul 2>&1
-    if %errorLevel% == 0 (
-        echo Installing ECEL for windows...
-    ) else (
-        echo Please run cmd as an admin
-    )
 
 SET "ECEL_DIR=%cd%"
 SET OUTPUT_PREFIX=ECEL INSTALLER:
 SET OUTPUT_ERROR_PREFIX=%OUTPUT_PREFIX% ERROR:
 
-rem donwloand chocolatey (windows package manager)
+rem Download chocolatey (windows package manager. "apt-get")
+rem After initial download, ensure that chocolatey is installed in C:\ProgramData (or change to whatever directory it becomes installed in.
 IF NOT EXIST %ProgramData%\chocolatey ( @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin")
 
 set REQUIRED_PLUGINS=nmap,wireshark
-
 echo Installing dependencies (Snoopy will not be installed-Linux only)
+
 for %%p in ("%REQUIRED_PLUGINS:,=" "%") do choco install %%p
 
-rem these may need to be changed depending on your version of windows.
 rem Wireshark installs by default in C:\Program Files (64 bit directory), while Nmap installs in C:\Program Files x86 (32 bit directory)
+rem These may need to be changed depending on where chocolatey installs nmap/tshark on your machine. CHeck Program files/Program files x86.
 set path=%path%;%ProgramFiles%\Wireshark
 set path=%path%;%ProgramFiles(x86)%\Nmap
 
 echo Installing python dependencies
 
+rem it may be necessary to download some of these packages manually if pip install does not work.
+rem ensure these packages are installed in your site-packages folder for your current versoin of python.
 set PYTHON_DEPENDENCIES=virtualenv,enum34,psutil,netifaces,autopy
 for %%p in ("%PYTHON_DEPENDENCIES:,=" "%") do pip install %%p
 
@@ -33,7 +30,7 @@ for /D %%d in (.\plugins\collectors\*) do copy %%d\config.json.template %%d\conf
 
 echo %OUTPUT_PREFIX% Compiling parsers
 
-rem make sure path to javac command is up to date.
+rem make sure path to javac command is up to date. This is needed to compile parser code.
 set path=%path%;%ProgramFiles%\Java\jdk1.8.0_91\bin
 
 for /D %%d in (.\plugins\parsers\*) do if exist %%d\*.java (javac %%d\*.java)
@@ -48,7 +45,7 @@ SET /P answer=Would you like to run ECEL automatically on login? (y/n):
 IF %answer% == y (
     echo Adding ecel to start up...
     cd %ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup
-    echo TODO: Add batch script that executes ecel to the start up folder
+    echo TODO: Add batch script that executes ecel in the start up folder
 )
 
 IF %answer% == n (
