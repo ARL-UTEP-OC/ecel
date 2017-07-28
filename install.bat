5530366f730d6e4162bd7bb1afd2cc8646d6f0a7
Kevin Keenan <kpkeenan@miners.utep.edu>
2017-07-27 19:34:18 -0600

    Linked to Issue #27
    Ability to start ecel on start up/remove ecel from start up.
    Bug-Nmap/Tshark wont run because they dont exist on the %PATH% variable at this point (they get added to %PATH% when install script is run.)

--- install.bat
+++ install.bat
@@ -3,6 +3,8 @@ REM Make sure path to pip is set correctly and CMD.exe is running with administr
 SET "ECEL_DIR=%cd%"
 SET OUTPUT_PREFIX=ECEL INSTALLER:
 SET OUTPUT_ERROR_PREFIX=%OUTPUT_PREFIX% ERROR:
+SET START_UP_DIR=%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup
+SET START_UP_FILE=%START_UP_DIR%\ecel_startup.bat
 
 rem Download chocolatey (windows package manager. "apt-get")
 rem After initial download, ensure that chocolatey is installed in C:\ProgramData (or change to whatever directory it becomes installed in.
@@ -15,8 +17,10 @@ for %%p in ("%REQUIRED_PLUGINS:,=" "%") do choco install %%p
 
 rem Wireshark installs by default in C:\Program Files (64 bit directory), while Nmap installs in C:\Program Files x86 (32 bit directory)
 rem These may need to be changed depending on where chocolatey installs nmap/tshark on your machine. CHeck Program files/Program files x86.
-set path=%path%;%ProgramFiles%\Wireshark
-set path=%path%;%ProgramFiles(x86)%\Nmap
+SET WIRESHARK_DIR=%ProgramFiles%\Wireshark
+SET NMAP_DIR=%ProgramFiles(x86)%\Nmap
+set path=%path%;%WIRESHARK_DIR%
+set path=%path%;%NMAP_DIR%
 
 echo Installing python dependencies
 
@@ -28,11 +32,8 @@ for %%p in ("%PYTHON_DEPENDENCIES:,=" "%") do pip install %%p
 echo Creating Plugin Configs
 for /D %%d in (.\plugins\collectors\*) do copy %%d\config.json.template %%d\config.json & copy %%d\config_schema.json.template %%d\config_schema.json
 
-:prompt
-::Clear the value of answer ready for use.
-SET JAVAC_DIR=
-SET /P JAVAC_DIR=Please enter the location of a 'javac' executable. (i.e. C:\Program Files\Java\jdk1.x.x_xx\bin)
-rem make sure path to javac command is up to date. This is needed to compile parser code.
+rem Enter the location of a javac executable below. This is needed to compile parser code.
+SET JAVAC_DIR=C:\Program Files\Java\jdk1.8.0_91\bin
 
 echo %OUTPUT_PREFIX% Compiling parsers
 
@@ -47,19 +48,13 @@ javac -cp .\plugins\parsers\nmap\java_classes\*.java
 SET answer=
 SET /P answer=Would you like to run ECEL automatically on login? (y/n):
 
-IF %answer% == y (c
-    echo Adding ecel to start up...
-    cd %ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup
-    echo TODO: Add batch script that executes ecel in the start up folder
+IF %answer% == y (
+    echo python %ECEL_DIR%\ecel_gui.py > "%START_UP_FILE%"
 )
 
 IF %answer% == n (
-    echo Remove ecel from start up...
-    cd %ProgramData%\Microsoft\Windows\Start Menu\Programs\
-    echo TODO: Remove ecel batch script from start up folder
+    del %START_UP_FILE%
 )
-
-cd %ECEL_DIR%
 
 echo For windows execution, the following must be installed manually: PyGobject, Gtk+ runtime, appindicator3
 echo %OUTPUT_PREFIX% Installation Complete. Type "python ecel_gui.py" to start ECEL
