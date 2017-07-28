5903f304868b6e9a8400054322cf75fa177aa32b
Kevin Keenan <kpkeenan@miners.utep.edu>
2017-07-27 23:20:08 -0600

    Linked to Issue #27
    Nmap parser functionality fully enabled.

--- plugins/parsers/nmap/nmap_parser.bat
+++ plugins/parsers/nmap/nmap_parser.bat
@@ -4,4 +4,6 @@ set HOME_DIR=%3
 
 set class_path=%HOME_DIR%\plugins\parsers\nmap\java_classes
 
-for %%x in (%xmlFilePath%\*.xml) do java -cp %class_path% NmapDataParser %%x %outputFilePath%\test.JSON
+for %%x in (%xmlFilePath%\*.xml) do (
+ java -cp %class_path% NmapDataParser %%x %outputFilePath%\%%~nx.JSON
+)
