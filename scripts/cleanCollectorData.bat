70f4f26fe4012869d0f042fddad74e06f78392ed
Kevin Keenan <kpkeenan@miners.utep.edu>
2017-08-04 20:03:59 -0600

    Force delete on cleanCollectorData.bat so that it skips the prompt.

--- scripts/cleanCollectorData.bat
+++ scripts/cleanCollectorData.bat
@@ -2,8 +2,8 @@ SET "ECEL_DIR=%cd%"
 SET COLLECTOR_DIR=%ECEL_DIR%\plugins\collectors\*
 
 FOR /D  %%d IN (%COLLECTOR_DIR%) DO (
-    del %%d\raw\*
-    del %%d\raw\META\*
-    del %%d\parsed\*
-    del %%d\compressed\*
+    del /F /Q %%d\raw\*
+    del /F /Q %%d\raw\META\*
+    del /F /Q %%d\parsed\*
+    del /F /Q %%d\compressed\*
 )
