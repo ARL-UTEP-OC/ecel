39f3e598923bb4e9e3c24cfffac0207c9a5fa16d
dreyes15 <dreyes15@miners.utep.edu>
2017-11-09 14:24:57 -0500

    Added export method from export_gui.py to engine.py file, removed GUI dependencies. Ready to test

--- engine/engine.py
+++ engine/engine.py
@@ -46,20 +46,17 @@ class Engine(object):
         subprocess.call(remove_cmd)  # TODO: Change this to not call external script
         os._exit(0)
 
-    #TODO: TEST, method from main_gui.py
+
     def stopall_collectors(self):
         for collector in self.collectors:
             if collector.is_enabled():
                 collector.terminate()
-            # if not pb.emit("delete-event", Gdk.Event(Gdk.DELETE)):
-            # pb.destroy()
 
     #TODO: TEST, method from main_gui.py
     def parse_all(self):
         for collector in self.collectors:
             collector.parser.parse()
-            pb.setValue(i/len(self.collectors))
-            pb.pbar.set_text("Parsing " + collector.name)
+            print "Parsing " + collector.name
 
     #TODO: TEST, method from main_gui.py
     def parser(self, collector):
@@ -71,19 +68,21 @@ class Engine(object):
 
     #TODO: Remove GUI dependencies, and test
     def export(self):
-        export_base_dir = self.entry_selected_folder.get_text()
-        export_raw = self.checkbutton_export_raw.get_active()
-        export_compressed = self.checkbutton_export_compressed.get_active()
-        export_parsed = self.checkbutton_export_parsed.get_active()
+        export_base_dir = '/root/Documents/ecel/export'
+        export_raw = True
+        export_compressed = True
+        export_parsed = True
+        compress_export = True
+        compress_export_format
 
-        if not export_base_dir:
-            utils.gui.show_error_message(self, "Please select a directory to export to.")
+        if export_base_dir == '':
+            print "Please select a directory to export to."
             return
         if not os.path.isdir(export_base_dir):
-            utils.gui.show_error_message(self, "Please select a valid directory to export to.")
+            print "Please select a valid directory to export to."
             return
         if not export_raw and not export_compressed and not export_parsed:
-            utils.gui.show_error_message(self, "Please select at least one data type to export.")
+            print "Please select at least one data type to export."
             return
 
         export_dir = os.path.join(export_base_dir, definitions.PLUGIN_COLLECTORS_EXPORT_DIRNAME.replace(
@@ -94,11 +93,6 @@ class Engine(object):
         os.makedirs(export_raw_dir)
         os.makedirs(export_compressed_dir)
         os.makedirs(export_parsed_dir)
-
-        progress = 0
-        pb = ProgressBar()
-        while Gtk.events_pending():
-            Gtk.main_iteration()
 
         for plugin in next(os.walk(self.collectors_dir))[1]:
             plugin_export_raw_dir = os.path.join(export_raw_dir, plugin)
@@ -115,34 +109,22 @@ class Engine(object):
                 shutil.copytree(plugin_collector_compressed_dir, plugin_export_compressed_dir)
             if export_parsed and os.path.exists(plugin_collector_parsed_dir) and os.listdir(plugin_collector_parsed_dir):
                 shutil.copytree(plugin_collector_parsed_dir, plugin_export_parsed_dir)
-            pb.setValue((progress / len(next(os.walk(self.collectors_dir))[1]))*.8)
-            pb.pbar.set_text("Copying files " + plugin)
-            while Gtk.events_pending():
-                Gtk.main_iteration()
-            progress += 1
+            print "Copying files " + plugin
 
-        if self.checkbutton_compress_export.get_active():
+        #Compress export just checks what way to export zip or tar
+        if compress_export:
             export_dir_notime = os.path.join(export_base_dir, definitions.PLUGIN_COLLECTORS_EXPORT_DIRNAME.replace(
                 definitions.TIMESTAMP_PLACEHOLDER, ""))
-            pb.pbar.set_text("Compressing data to " + export_dir)
-            pb.setValue(.85)
-            while Gtk.events_pending():
-                Gtk.main_iteration()
-            if self.radiobutton_compress_export_format_zip.get_active():
+
+            print "Compressing data to " + export_dir
+
+            if compress_export_format == 'zip':
                 zip(export_dir, export_dir_notime)
-            elif self.radiobutton_compress_export_format_tar.get_active():
+            elif compress_export_format == 'tar':
                 tar(export_dir, export_dir_notime)
-            pb.pbar.set_text("Cleaning up " + export_dir)
-            pb.setValue(.9)
-            while Gtk.events_pending():
-                Gtk.main_iteration()
-            shutil.rmtree(export_dir)
-        if not pb.emit("delete-event", Gdk.Event()):
-            pb.destroy()
+            print "Cleaning up " + export_dir
 
-        utils.gui.show_alert_message(self, "Export complete")
-        self.hide()
-
+        print "Export complete"
 
     def get_collector(self, name):
         return next(p for p in self.collectors if p.name == name)
